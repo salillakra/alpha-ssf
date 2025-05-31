1f88a796b3d0b42d906992d6790b4b599c552c09
Salil Lakra <salillakra.dev@gmail.com>
2025-05-31 06:12:34 +0530

    Refactor layout.tsx to integrate ThemeProvider and Toaster for improved theming and notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ThemeProvider from "@/components/theme-provider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +27,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased suppressHydrationWarning`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+            <Toaster />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
